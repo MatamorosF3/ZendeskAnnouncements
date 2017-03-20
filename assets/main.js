dd81a3a124441e70cedfb27cc5c91425c4f65de6
Francisco Matamoros <fmatamoros@FMATAMOROS-ISTIT.local>
2017-03-20 11:04:47 -0600

    deleted unnecessary files and updated logos

--- assets/main.js
+++ assets/main.js
@@ -1,68 +1,108 @@
-var client;
-var data2;
+    var client = ZAFClient.init();
+    requestAnnouncementsInfo();
 
-$(function() {
-    client = ZAFClient.init();
+    function showInfo(data) {
 
-    // client.invoke('resize', {
-    //     width: '100%',
-    //     height: '120px'
-    // });
-    requestIssueInfo(client);
-    // client.get('ticket.requester.id').then(function(data) {
-    //     requestUserInfo(client);
-    // });
-});
+        var context = {};
+        var rows = [];
 
-function showInfo(data) {
+        Object.keys(data).forEach(function(key) {
+            if (data[key].display === 1 || data[key].display === '1') {
+                rows.push({ title: data[key].title.replace('<br>', '\n'), detail: data[key].detail.replace('<br>', '\n'), id: key.toString() });
+            }
+        });
 
-    var context = {};
-    var rows = [];
+        context.items = rows;
+        Handlebars.registerHelper('data_rows', function() {
+            var detail = Handlebars.escapeExpression(this.detail),
+                title = Handlebars.escapeExpression(this.title);
+            return new Handlebars.SafeString(
+                "<td><a onclick=getModal('" + this.id.toString() + "') style=cursor:pointer; >" + this.title + "</a></td> <td>" + this.detail
+            );
+        });
 
-    for (i in data2) {
-        rows.push({ organization: data2[i].ORGANIZATION, issue: data2[i].ISSUE, id: data2[i].ID });
+        
+
+        var source = $("#announcements-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(context);
+        $("#content").html(html);
     }
 
-    context.items = rows;
+    function showError() {
+        var error_data = {
+            'status': 404,
+            'statusText': 'Not found'
+        };
+        var source = $("#error-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(error_data);
+        $("#content").html(html);
+    }
 
-    Handlebars.registerHelper('data_rows', function() {
-        var issue = Handlebars.escapeExpression(this.issue),
-            organization = Handlebars.escapeExpression(this.organization);
-        return new Handlebars.SafeString(
-            "<td><a onclick=getModal(" + this.id + ") style=cursor:pointer; >" + this.organization + "</a></td> <td>" + this.issue
-        );
-    });
+    function requestAnnouncementsInfo() {
 
-    var source = $("#requester-template").html();
-    var template = Handlebars.compile(source);
-    var html = template(context);
-    $("#content").html(html);
-}
+        var settings = {
+            url: 'http://ec2-52-40-155-63.us-west-2.compute.amazonaws.com:3000/announcement',
+            type: 'GET',
+            dataType: 'json'
+        };
 
-function showError() {
-    var error_data = {
-        'status': 404,
-        'statusText': 'Not found'
-    };
-    var source = $("#error-template").html();
-    var template = Handlebars.compile(source);
-    var html = template(error_data);
-    $("#content").html(html);
-}
+        client.request(settings).then(function(data) {
+                showInfo(data)
+            },
+            function(response) {
+                showError();
+            });
+    }
 
-function requestIssueInfo(client) {
+    function getModal(id) {
 
-    var settings = {
-        url: 'http://ec2-54-159-13-160.compute-1.amazonaws.com:9000/issue',
-        type: 'GET',
-        dataType: 'json'
-    };
+        localStorage.setItem('id', id);
+        client.invoke('instances.create', {
+            location: 'modal',
+            url: "assets/showAnnouncement.html"
+        }).then(function(modalContext) {
 
-    client.request(settings).then(function(data) {
-            data2 = data;
-            showInfo(data2)
-        },
-        function(response) {
-            showError();
+            var modalClient = client.instance(modalContext['instances.create'][0].instanceGuid);
+
+            modalClient.invoke('resize', {
+                width: '80vw',
+                height: '80vh'
+            });
+
+            modalClient.on('announcementUpdated', function(data) {
+                requestAnnouncementsInfo();
+                Materialize.toast('Announcement updated.', 3000, 'rounded');
+            });
         });
-}
+    }
+
+
+    function getModalCreateAnnouncement() {
+
+        client.invoke('instances.create', {
+            location: 'modal',
+            url: "assets/createAnnouncement.html"
+        }).then(function(modalContext) {
+            var modalClient = client.instance(modalContext['instances.create'][0].instanceGuid);
+
+
+            modalClient.invoke('resize', {
+                width: '80vw',
+                height: '80vh'
+            });
+
+            modalClient.on('modal.close', function() {
+
+                console.log("Modal is closed");
+            });
+
+            modalClient.on('announcementCreated', function() {
+                Materialize.toast('Announcement created.', 3000, 'rounded');
+                requestAnnouncementsInfo();
+            });
+
+        });
+
+    }
